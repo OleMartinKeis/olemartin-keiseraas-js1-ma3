d568ea98ea70edb9808601e67e2dc7b86b635127
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2022-08-26 13:47:46 +0200

    Added js and html

---
+++ javascript/question-1.js
@@ -0,0 +1,5 @@
+const getRemainder = (a, b) => a % b;
+
+const result = getRemainder(10, 5);
+
+console.log(result)
---
+++ javascript/question-2.js
@@ -0,0 +1,16 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=f8bd76cbac4e4b2bbc298b6a97e55095"
+
+async function getGames() {
+
+    const response = await fetch(url);
+    const results = await response.json();
+    console.log(results)
+
+    const gamesList = results.all;
+
+    for(let i = 0; i < gamesList.length; i++) {
+        console.log(gamesList[i].text);
+    }
+}
+
+getGames()
